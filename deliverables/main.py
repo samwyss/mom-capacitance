f24856ef62fad4553172d31b4846a69e42fd0ded
Sam Wyss <sjwyss@outlook.com>
2024-04-29 15:07:33 -0400

    added deliverables folder

---
+++ deliverables/main.py
@@ -0,0 +1,94 @@
+from Solver import Solver
+
+PHI = 1
+SIDE_LEN = 1e-2
+
+
+def main():
+    # part 1
+    print("Part 1")
+    solver_1_10 = Solver(10, SIDE_LEN, 1, PHI)
+    solver_1_20 = Solver(20, SIDE_LEN, 1, PHI)
+    solver_1_30 = Solver(30, SIDE_LEN, 1, PHI)
+    """
+    solver_1_40 = Solver(40, SIDE_LEN, 1, PHI)
+    solver_1_50 = Solver(50, SIDE_LEN, 1, PHI)
+    solver_1_60 = Solver(60, SIDE_LEN, 1, PHI)
+    solver_1_70 = Solver(70, SIDE_LEN, 1, PHI)
+    solver_1_80 = Solver(80, SIDE_LEN, 1, PHI)
+    solver_1_90 = Solver(90, SIDE_LEN, 1, PHI)
+    solver_1_100 = Solver(100, SIDE_LEN, 1, PHI)
+    """
+    solver_1_10.solve()
+    solver_1_20.solve()
+    solver_1_30.solve()
+    """
+    solver_1_40.solve()
+    solver_1_50.solve()
+    solver_1_60.solve()
+    solver_1_70.solve()
+    solver_1_80.solve()
+    solver_1_90.solve()
+    solver_1_100.solve()   
+    """
+    print("\n")
+
+    print("Part 2")
+    solver_2_10 = Solver(10, SIDE_LEN, 2, PHI)
+    solver_2_20 = Solver(20, SIDE_LEN, 2, PHI)
+    solver_2_30 = Solver(30, SIDE_LEN, 2, PHI)
+    """
+    solver_2_40 = Solver(40, SIDE_LEN, 2, PHI)
+    solver_2_50 = Solver(50, SIDE_LEN, 2, PHI)
+    solver_2_60 = Solver(60, SIDE_LEN, 2, PHI)
+    solver_2_70 = Solver(70, SIDE_LEN, 2, PHI)
+    solver_2_80 = Solver(80, SIDE_LEN, 2, PHI)
+    solver_2_90 = Solver(90, SIDE_LEN, 2, PHI)
+    solver_2_100 = Solver(100, SIDE_LEN, 2, PHI)
+    """
+    solver_2_10.solve()
+    solver_2_20.solve()
+    solver_2_30.solve()
+    """
+    solver_2_40.solve()
+    solver_2_50.solve()
+    solver_2_60.solve()
+    solver_2_70.solve()
+    solver_2_80.solve()
+    solver_2_90.solve()
+    solver_2_100.solve()
+    """
+    print("\n")
+
+    # part 3
+    print("Part 3")
+    solver_3_10 = Solver(10, SIDE_LEN, 3, PHI)
+    solver_3_20 = Solver(20, SIDE_LEN, 3, PHI)
+    solver_3_30 = Solver(30, SIDE_LEN, 3, PHI)
+    """
+    solver_3_40 = Solver(40, SIDE_LEN, 3, PHI)
+    solver_3_50 = Solver(50, SIDE_LEN, 3, PHI)
+    solver_3_60 = Solver(60, SIDE_LEN, 3, PHI)
+    solver_3_70 = Solver(70, SIDE_LEN, 3, PHI)
+    solver_3_80 = Solver(80, SIDE_LEN, 3, PHI)
+    solver_3_90 = Solver(90, SIDE_LEN, 3, PHI)
+    solver_3_100 = Solver(100, SIDE_LEN, 3, PHI)
+    """
+    solver_3_10.solve()
+    solver_3_20.solve()
+    solver_3_30.solve()
+    """
+    solver_3_40.solve()
+    solver_3_50.solve()
+    solver_3_60.solve()
+    solver_3_70.solve()
+    solver_3_80.solve()
+    solver_3_90.solve()
+    solver_3_100.solve()
+    """
+    print("\n")
+
+
+# call main function after declaration
+if __name__ == "__main__":
+    main()
